dcf15ed8c28e82b7f38e3bf3e7f5fa89e3627337
K1DV5 <kidusadugna@gmail.com>
2025-08-05 00:12:54 +0200

    show last updated at the bottom of pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
             editLink: {
                 baseUrl: 'https://github.com/wuchalejs/wuchalejs.github.io/edit/main/',
             },
+            lastUpdated: true,
             social: [
                 { icon: 'github', label: 'GitHub', href: 'https://github.com/wuchalejs/wuchale' },
                 { icon: 'npm', label: 'NPM', href: 'https://npmjs.com/package/wuchale' },
