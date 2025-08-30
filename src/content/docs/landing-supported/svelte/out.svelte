02f95d1cda581a76e0f9605a5ffdca60bd07e57a
K1DV5 <kidusadugna@gmail.com>
2025-08-30 13:26:05 +0200

    small fix in svelte snippet in landing

--- src/content/docs/landing-supported/svelte/out.svelte
+++ src/content/docs/landing-supported/svelte/out.svelte
@@ -9,6 +9,6 @@
 <div>
 	<p>{_w_runtime_.t(0)}</p>
 	<p title={_w_runtime_.t(1)} class="para">
-		<W_tx_ x={_w_runtime_().cx(2)} a={[username]} />
+		<W_tx_ x={_w_runtime_.cx(2)} a={[username]} />
 	</p>
 </div>
