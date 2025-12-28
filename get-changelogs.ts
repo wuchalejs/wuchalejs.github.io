8cbf402aaaa44638c914715974d2ef6368dc278b
K1DV5 <kidusadugna@gmail.com>
2025-12-28 20:43:35 +0100

    create changelogs dir first

--- get-changelogs.ts
+++ get-changelogs.ts
@@ -1,6 +1,6 @@
 // $ node %f
 
-import { writeFile } from 'node:fs/promises'
+import { mkdir, writeFile } from 'node:fs/promises'
 
 const packages = [
     ['wuchale', 'wuchale'],
@@ -13,5 +13,6 @@ for (const [name, packageName] of packages) {
     const url = `https://raw.githubusercontent.com/wuchalejs/wuchale/refs/heads/main/packages/${name}/CHANGELOG.md`
     const res = await fetch(url)
     const content = await res.text()
+    await mkdir('./src/content/docs/changelogs', {recursive: true})
     await writeFile(`./src/content/docs/changelogs/${name}.md`, `---\ntitle: "${packageName}"\n---\n${content}`)
 }
