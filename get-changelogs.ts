302a3120556cf7bbcfb84737d3b2adfc74acba68
K1DV5 <kidusadugna@gmail.com>
2026-01-04 18:54:06 +0100

    single package name as title in changelogs

--- get-changelogs.ts
+++ get-changelogs.ts
@@ -13,7 +13,10 @@ const packages = [
 for (const [name, packageName] of packages) {
     const url = `https://raw.githubusercontent.com/wuchalejs/wuchale/refs/heads/main/packages/${name}/CHANGELOG.md`
     const res = await fetch(url)
-    const content = await res.text()
+    let content = await res.text()
+    if (content.startsWith('# ')) {
+        content = content.slice(content.indexOf('\n') + 1)
+    }
     await mkdir('./src/content/docs/changelogs', {recursive: true})
     await writeFile(`./src/content/docs/changelogs/${name}.md`, `---\ntitle: "${packageName}"\n---\n${content}`)
 }
